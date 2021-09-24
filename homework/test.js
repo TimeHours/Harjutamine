5d8e114786d19515af34043a345418e8507f5832
TimeHour <90251388+TimeHour@users.noreply.github.com>
2021-09-24 11:13:59 +0300

    mingid muudatused

---
+++ homework/test.js
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    </ul>
+          <li>Crossroad Gardens, 88</li>
+          <li>Strawberry Mountain lands, 88</li>
+          <li>Weeping Willow Acres, 88</li>            
+
+
+
+
+
