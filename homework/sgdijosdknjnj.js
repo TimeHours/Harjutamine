30c0ed10e0da8609c9a64cba6f64330386e17609
TimeHours <90027900+TimeHours@users.noreply.github.com>
2021-11-24 15:01:34 +0200

    a

--- homework/sgdijosdknjnj.js
+++ homework/sgdijosdknjnj.js
@@ -1,8 +1,4 @@
-for (let line of file) {
-    let name = "asd", serial = "12354654"
-    let sql = "('"+ serial +"','"+name+"'),\n";
-    fs.appendFile('fail.txt', sql)
-}
+
 
 
 
@@ -11,6 +7,42 @@ for (let line of file) {
 let sql = "INSERT INTO table (serial, name) VALUES "
 for (let line of line) {
   let name = "asd", serial = "12354654"
-  sqlv += "('"+ serial +"','"+name+"'),\n";
+  sql += "('"+ serial +"','"+name+"'),\n";
   }
 fs.writeFile('fail.txt', sql)
+
+
+
+
+
+
+
+fs = require('fs');
+fs.readFile('/Users/MM-21-152/Desktop/LE.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  fs.appendFile('fgal.txt', sql)
+  data.replace("data","")
+  .replace("data","").replace("data","")
+  .replace("data","")
+
+});
+
+
+
+
+
+
+
+
+
+
+
+data.split("\n")
+
+for (let line of Array) {
+  let name = "Valjuveljed", serial = "00002356517"
+  let sql = "('"+ serial +"','"+name+"'),\n";
+  fs.appendFile('LE.txt', sql)
+}
